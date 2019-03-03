588e01a0c9ba67f638cadcb19d469a270c6ccf3c
reubenls <reubenls@outlook.com>
2019-03-03 20:07:23 +1300

    some text and label updates

--- cloud_test_app/questionnaire/forms.py
+++ cloud_test_app/questionnaire/forms.py
@@ -6,3 +6,7 @@ class QuestionnaireForm(forms.ModelForm):
     class Meta:
         model = Answers
         fields = ['fav_month', 'fav_day']
+        labels = {
+            "fav_month": "What is your favourite month?",
+            "fav_day": "What is your favourite day?"
+        }
